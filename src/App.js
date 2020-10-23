0284be810d07826e009a0308855a25111c660321
AndriiLiashenko-Perfectial <andrii.liashenko@perfectial.com>
2020-10-23 16:13:08 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState({
+    name: "Andrii",
+    lastName: 'Liashenko'
+  });
+
+  const [newName, setNewName] = useState({
+    name: "",
+    lastName: ''
+  });
+
+  const onChangeName = (e) =>{
+    console.log(newName)
+    e.preventDefault()
+    const {name,lastName} = newName;
+      setName((state)=>{
+        return {
+          ...state,
+          name,
+          lastName
+        }
+      })
+
+      setNewName({
+        name: '',
+        lastName: ''
+      })
+
+  }
+
+const setNewNameFunc = (e)=>{
+  setNewName((state)=>{
+    return {
+      ...state,
+      [e.target.name]: e.target.value
+    }
+  })
+}  
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name.name} {name.lastName}</h1> 
+      <form onSubmit={onChangeName}>
+        <input type="text" name='name' value={newName.name} onChange={setNewNameFunc}/>
+        <input type="text" name='lastName' value={newName.lastName} onChange={setNewNameFunc}/>
+        <button type='submit' onClick={onChangeName}>Submit</button>
+
+      </form>
     </div>
   );
 }
